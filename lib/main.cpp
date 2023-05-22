867dd019f40942547fb4ece5b623032ee07df24c
Eli Bajec-Dickinson <tacoman164@gmail.com>
2023-05-22 12:24:11 -0400

    dd

--- lib/main.cpp
+++ lib/main.cpp
@@ -8,13 +8,13 @@ using namespace std;
 int main()
 {
 
-	Matrix<3, 2> A = {
+	matrix<3, 2> A = {
 		73, 73, 
 		69, 69,
 		0, 1
 	};
 
-	Matrix<3, 1> B = {1, 1, 1};
+	matrix<3, 1> B = {1, 1, 1};
 
 	(A.kronecker_prod(B)).print();
 
--- lib/matrix.hpp
+++ lib/matrix.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
+
 #include <initializer_list>
 #include <vector>
 #include <type_traits>
@@ -8,41 +9,41 @@
 using namespace std;
 
 template <int m, int n, typename F = double>
-class Matrix
+class matrix
 {
 protected:
     F mem[m * n] = {0};
 
 public:
-    Matrix();
-    Matrix(const Matrix<m, n, F> &);
-    Matrix(F *new_data);
-    Matrix(const vector<vector<F>> &arr);
-    Matrix(const initializer_list<F> &arr);
+    matrix();
+    matrix(const matrix<m, n, F> &);
+    matrix(F *new_data);
+    matrix(const vector<vector<F>> &arr);
+    matrix(const initializer_list<F> &arr);
 
     inline F *operator[](int i) { return mem + i * n; }
     inline const F *operator[](int i) const { return mem + i * n; }
-    Matrix<m, n, F> operator+(const Matrix<m, n, F> &B) const;
-    Matrix<m, n, F> operator-(const Matrix<m, n, F> &B) const;
-    Matrix<m, n, F> operator*(const F &c) const;
+    matrix<m, n, F> operator+(const matrix<m, n, F> &B) const;
+    matrix<m, n, F> operator-(const matrix<m, n, F> &B) const;
+    matrix<m, n, F> operator*(const F &c) const;
     template <int l>
-    Matrix<m, l, F> operator*(const Matrix<n, l, F> &B) const;
-    Matrix<m, n, F> operator^(const int &pow) const;
+    matrix<m, l, F> operator*(const matrix<n, l, F> &B) const;
+    matrix<m, n, F> operator^(const int &pow) const;
     /*Creates a new matrix by placing the two inputs side by side.*/
     template <int l>
-    Matrix<m, n + l, F> operator|(const Matrix<m, l, F> &other) const;
-    bool operator==(const Matrix<m, n, F> &other) const;
-    bool operator!=(const Matrix<m, n, F> &other) const;
+    matrix<m, n + l, F> operator|(const matrix<m, l, F> &other) const;
+    bool operator==(const matrix<m, n, F> &other) const;
+    bool operator!=(const matrix<m, n, F> &other) const;
 
     template <typename T>
-    operator Matrix<m, n, T>() const
+    operator matrix<m, n, T>() const
     {
         T new_data[m * n];
         for (int i = 0; i < m * n; i++)
         {
             new_data[i] = static_cast<T>(mem[i]);
         }
-        return Matrix<m, n, T>(new_data);
+        return matrix<m, n, T>(new_data);
     }
 
     F *data() { return &(this->mem[0]); }
@@ -50,20 +51,20 @@ public:
     int rows() { return m; }
     int cols() { return n; }
 
-    Matrix<1, n, F> row(int i) const;
-    Matrix<m, 1, F> col(int j) const;
-    Matrix<m - 1, n - 1, F> submatrix(int i, int j) const;
-    Matrix<n, m, F> transpose() const;
+    matrix<1, n, F> row(int i) const;
+    matrix<m, 1, F> col(int j) const;
+    matrix<m - 1, n - 1, F> submatrix(int i, int j) const;
+    matrix<n, m, F> transpose() const;
     template <int k, int l>
-    Matrix<m + k, n + l, F> direct_sum(const Matrix<k, l, F> &other) const;
+    matrix<m + k, n + l, F> direct_sum(const matrix<k, l, F> &other) const;
     template <int k, int l>
-    Matrix<m*k,n*l,F> kronecker_prod(const Matrix<k,l,F> &other) const;
+    matrix<m*k,n*l,F> kronecker_prod(const matrix<k,l,F> &other) const;
 
-    static Matrix<m, n, F> id();
+    static matrix<m, n, F> id();
 
     // Don't really know how to avoid implementing this outside of class.
     // This is so you can multiply from the right as well.
-    friend Matrix<m, n, F> operator*(const F &c, const Matrix<m, n, F> &B)
+    friend matrix<m, n, F> operator*(const F &c, const matrix<m, n, F> &B)
     {
         F new_data[m * n];
 
@@ -72,14 +73,14 @@ public:
             new_data[i] = B.mem[i] * c;
         }
 
-        return Matrix<m, n>(new_data);
+        return matrix<m, n>(new_data);
     }
 };
 
 // needed to do this for base cases if matrix size gets reduced in recursive
 // function call
 template <int n, typename F>
-class Matrix<0, n, F>
+class matrix<0, n, F>
 {
 public:
     F *operator[](int i) const
@@ -87,11 +88,11 @@ public:
         return nullptr;
     }
     F *data() { return nullptr; }
-    Matrix<0, n, F> submatrix(int i, int j) const { return Matrix<0, n>(); }
+    matrix<0, n, F> submatrix(int i, int j) const { return matrix<0, n>(); }
 };
 
 template <int n, typename F>
-class Matrix<n, 0, F>
+class matrix<n, 0, F>
 {
 public:
     F *operator[](int i) const
@@ -99,11 +100,11 @@ public:
         return nullptr;
     }
     F *data() { return nullptr; }
-    Matrix<n, 0, F> submatrix(int i, int j) const { return Matrix<n, 0>(); }
+    matrix<n, 0, F> submatrix(int i, int j) const { return matrix<n, 0>(); }
 };
 
 template <typename F>
-class Matrix<0, 0, F>
+class matrix<0, 0, F>
 {
 public:
     F *operator[](int i) const
@@ -111,13 +112,13 @@ public:
         return nullptr;
     }
     F *data() { return nullptr; }
-    Matrix<0, 0, F> submatrix(int i, int j) const { return Matrix<0, 0>(); }
+    matrix<0, 0, F> submatrix(int i, int j) const { return matrix<0, 0>(); }
 };
 
 /*****NON MEMBER STUFF*****/
 
 template <int n, typename F>
-F det_laplace(Matrix<n, n, F> A);
+F det_laplace(matrix<n, n, F> A);
 
 #include "matrix.tpp"
 
